9abdc073e94ec599c57944aa7fc80b334f456355
95sid <nobbie7579@gmail.com>
2024-09-23 10:56:23 +0530

    practicing

---
+++ 02/02/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "./Title";
+
+const App = () => {
+  return (
+    <div>
+      <Title name={"Siddhant"} />
+      <Title name={"Abhinav"} />
+      <Title name={"Mohit"} />
+    </div>
+  );
+};
+
+export default App;
